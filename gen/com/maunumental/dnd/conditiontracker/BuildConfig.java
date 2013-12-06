0cc473df7f55b38880b25351b246ae8d9a9c762d
Mike Maunu <mmaunu@francisparker.org>
2013-12-05 21:53:00 -0800

    What a mess...seriously, stay away :)

---
+++ gen/com/maunumental/dnd/conditiontracker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.maunumental.dnd.conditiontracker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
